9dc026e2f5fabce9a54210e1e0b997a03415c240
scot.itakura <scot.itakura@gmail.com>
2023-03-02 12:29:13 -0800

    Setup Snowflake connection

--- earthquake_api.py
+++ earthquake_api.py
@@ -1,25 +1,38 @@
 from urllib.request import urlopen
 import json
 import os
-import snowflake
+import snowflake.connector
 
 
 url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
 response = urlopen(url)
 data_json = json.loads(response.read())
 
-eq_properties = data_json['features'][0]['properties']
-
 ctx = snowflake.connector.connect(
     user='scotitakura',
     password='Neokiki01!',
-    account='TP35935'
+    account='cd28840.us-east4.gcp',
+    warehouse='GEO_PROJECT',
+    database='GEO_RAW',
+    schema='GEO_SCHEMA'
     )
 cs = ctx.cursor()
-try:
-    cs.execute("SELECT current_version()")
-    one_row = cs.fetchone()
-    print(one_row[0])
-finally:
-    cs.close()
+
+for event in data_json['features']:
+    eq_time = event['properties']['time']
+    eq_mag = event['properties']['mag']
+    eq_place = event['properties']['place']
+    eq_sources = event['properties']['sources']
+    eq_type = event['properties']['type']
+    eq_coordinates = event['geometry']['coordinates']
+
+    try:
+        sql_statement = "INSERT INTO eq_table (eq_time, magnitude, place, sources, type, coordinates) VALUES (" + "'" + str(eq_time) + "', " + str(eq_mag) + ", '" + eq_place + "', '" + eq_sources + "', '" + eq_type + "', '" + str(eq_coordinates) + "')"
+        cs.execute(sql_statement)
+        one_row = cs.fetchone()
+    except:
+        print('didnt work', sql_statement)
+
+
+cs.close()
 ctx.close()
